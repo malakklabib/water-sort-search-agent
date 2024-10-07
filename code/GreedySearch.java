439cc417eb08b887f8fc34ad2eb3870f743fd3db
Omar Sobhy <omar.sobhy@rwth-aachen.de>
2024-10-07 09:20:23 +0300

    Refactored A* and Greedy

--- code/GreedySearch.java
+++ code/GreedySearch.java
@@ -1,30 +1,16 @@
 package code;
 
-import java.util.PriorityQueue;
+import java.util.*;
 
-public class GreedySearch implements SearchStrategy {
-    PriorityQueue<Node> pq;
-
-    public GreedySearch(int heuristic){
-        if(heuristic==1){
-            //implement comparable
-        }
-        else{
-            //implement another type of comparable
-        }
-    }
-    @Override
-    public void add(Node node) {
-        pq.add(node);
+public class GreedySearch extends BestFirstSearch {
+    // Changed arg `int heuristic` to `heuristic*Id*` to avoid confusing with newly added instance variable of same name
+    public GreedySearch(int heuristicId) {
+        super(heuristicId);
     }
 
+    // Nodes are compared using f(n) = h(n)
     @Override
-    public Node remove() {
-        return pq.remove();
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return pq.isEmpty();
+    public Comparator<Node> makeComparator() {
+        return (n1, n2) -> Integer.compare(heuristic.evaluate(n1), heuristic.evaluate(n2));
     }
 }
